60a7564e4eaeee07669ba0a080d3fe3e491f5e29
sujeethrao <sujithps137@gmail.com>
2017-01-16 14:13:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/sujeeth/videoplayer/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.sujeeth.videoplayer;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.videoview);
+
+        MediaController mc = new MediaController(this);
+        mc.setAnchorView(videoView);
+
+        Uri video_uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.naalo_nenu_promo);
+        videoView.setVideoURI(video_uri);
+        videoView.setMediaController(mc);
+        videoView.start();
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putInt("current position",videoView.getCurrentPosition() );
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if(savedInstanceState!=null){
+            int currentPos =  savedInstanceState.getInt("current position");
+            videoView.seekTo(currentPos);
+        }
+        super.onRestoreInstanceState(savedInstanceState);
+
+
+    }
+}
